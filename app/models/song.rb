e8e28954dcf7b1b791791be28cdd074177d19376
Prelang Builder <builder@prelang.com>
2015-08-23 09:48:08 +0000

    Made model 'Song' votable by adding acts_as_votable

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,4 +2,5 @@ class Song < ActiveRecord::Base
   belongs_to :artist
   validates_formatting_of :preview_url, using: :url
   validates_formatting_of :purchase_url, using: :url
+  acts_as_votable 
 end
